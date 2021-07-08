d40490c9865742ca5c4305704604ce298db7c5ac
Mac <mac@Macs-MacBook-Air.local>
2021-07-08 13:03:20 +0530

    first demo app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MaterialApp(
+  home:Scaffold(
+    appBar:AppBar(
+      title: Text('hey sidd'),
+      centerTitle: true,
+    ),
+    body :Center(child:Text('hellow world'),
+    ),
+     
+  ),
+
+)) ;
